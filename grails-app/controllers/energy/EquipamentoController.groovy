9411d35401d863cb4ea048fa7e2867f3be290fb3
Jeferson Machado <jefersonm@gmail.com>
2011-05-30 23:12:16 -0300

    add equipamente controller

---
+++ grails-app/controllers/energy/EquipamentoController.groovy
@@ -0,0 +1,6 @@
+package energy
+
+class EquipamentoController {
+
+    def index = { }
+}
---
+++ test/unit/energy/EquipamentoControllerTests.groovy
@@ -0,0 +1,17 @@
+package energy
+
+import grails.test.*
+
+class EquipamentoControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
